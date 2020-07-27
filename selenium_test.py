bb7ce561cfc056d73642efec35e0b00c93dc95cd
donghaeum <edh1462@gmail.com>
2020-07-27 18:15:51 +0900

    feat: Update file

--- selenium_test.py
+++ selenium_test.py
@@ -25,67 +25,67 @@ time.sleep(5)
 driver.get("https://www.instagram.com/explore/tags/%EC%9D%8C%EC%8B%9D/")
 
 time.sleep(5)
-#
-# SCROLL_PAUSE_TIME = 1.0
-# reallink = []
-#
-# while True:
-#     pageString = driver.page_source
-#     bsObj = BeautifulSoup(pageString, 'lxml')
-#
-#     for link1 in bsObj.find_all(name='div', attrs={"class":"Nnq7C weEfm"}):
-#         for i in range(3):
-#             title = link1.select('a')[i]
-#             real = title.attrs['href']
-#             reallink.append(real)
-#     last_height = driver.execute_script('return document.body.scrollHeight')
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     time.sleep(SCROLL_PAUSE_TIME)
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#
-#     if new_height == last_height:
-#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#         time.sleep(SCROLL_PAUSE_TIME)
-#         new_height = driver.execute_script("return document.body.scrollHeight")
-#
-#         if new_height == last_height:
-#             break
-#         # else:
-#         #     last_height = new_height
-#         #     continue
-#
-# num_of_data = len(reallink)
-#
-# print('총 {0}개의 데이터를 수집합니다.'.format(num_of_data))
-# csvtext = []
-#
-# for i in tqdm(range(num_of_data)):
-#
-#     csvtext.append([])
-#     req = Request("https://www.instagram.com/p"+reallink[i], headers={'User-Agent': 'Mozila/5.0'})
-#
-#     webpage = urlopen(req).read()
-#     soup = BeautifulSoup(webpage, 'lxml', from_encoding='utf-8')
-#     soup1 = soup.find('meta', attrs={'property':"og:description"})
-#
-#     reallink1 = soup1['content']
-#     reallink1 = reallink1[reallink1.find("@") + 1:reallink1.find(")")]
-#     reallink1 = reallink1[:20]
-#
-#     if reallink1 == '':
-#         reallink1 = "Null"
-#     csvtext[i].append(reallink1)
-#
-#     for reallink2 in soup.find_all('meta', attrs={'property':"instapp:hashtags"}):
-#         hashtags = reallink2['content'].rstrip(',')
-#         csvtext[i].append(hashtags)
-#
-#     # csv로 저장
-#
-#     data = pd.DataFrame(csvtext)
-#     data.to_csv('insta.txt', encoding='utf-8')
-#
-# driver.close()
+
+SCROLL_PAUSE_TIME = 1.0
+reallink = []
+
+while True:
+    pageString = driver.page_source
+    bsObj = BeautifulSoup(pageString, 'lxml')
+
+    for link1 in bsObj.find_all(name='div', attrs={"class":"Nnq7C weEfm"}):
+        for i in range(3):
+            title = link1.select('a')[i]
+            real = title.attrs['href']
+            reallink.append(real)
+    last_height = driver.execute_script('return document.body.scrollHeight')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(SCROLL_PAUSE_TIME)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+
+    if new_height == last_height:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        else:
+            last_height = new_height
+            continue
+
+num_of_data = len(reallink)
+
+print('총 {0}개의 데이터를 수집합니다.'.format(num_of_data))
+csvtext = []
+
+for i in tqdm(range(num_of_data)):
+
+    csvtext.append([])
+    req = Request("https://www.instagram.com/p"+reallink[i], headers={'User-Agent': 'Mozila/5.0'})
+
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'lxml', from_encoding='utf-8')
+    soup1 = soup.find('meta', attrs={'property':"og:description"})
+
+    reallink1 = soup1['content']
+    reallink1 = reallink1[reallink1.find("@") + 1:reallink1.find(")")]
+    reallink1 = reallink1[:20]
+
+    if reallink1 == '':
+        reallink1 = "Null"
+    csvtext[i].append(reallink1)
+
+    for reallink2 in soup.find_all('meta', attrs={'property':"instapp:hashtags"}):
+        hashtags = reallink2['content'].rstrip(',')
+        csvtext[i].append(hashtags)
+
+    # csv로 저장
+
+    data = pd.DataFrame(csvtext)
+    data.to_csv('insta.txt', encoding='utf-8')
+
+driver.close()
 
 # html = driver.page_source
 # soup = BeautifulSoup(html, 'html.parser')
